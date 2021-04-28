bba981bdbf92499ab6e5cabdc66c92a0a53cfe25
Enrique Flores <ehf.81@hotmail.com>
2021-04-27 17:47:04 -0700

    edit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const path = require("path");
+const { Pool } = require("pg");
+
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const sql_create = `CREATE TABLE IF NOT EXISTS Books (
+  Book_ID SERIAL PRIMARY KEY,
+  Title VARCHAR(100) NOT NULL,
+  Author VARCHAR(100) NOT NULL,
+  Comments TEXT
+);`;
+
+pool.query(sql_create, [], (err, result) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful creation of the 'Books' table");
+});
+
+console.log("Successful creation of the 'Books' table");
+
+// Database seeding
+const sql_insert = `INSERT INTO Books (Book_ID, Title, Author, Comments) VALUES
+    (1, 'Mrs. Bridge', 'Evan S. Connell', 'First in the serie'),
+    (2, 'Mr. Bridge', 'Evan S. Connell', 'Second in the serie'),
+    (3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne')
+  ON CONFLICT DO NOTHING;`;
+pool.query(sql_insert, [], (err, result) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  const sql_sequence =
+    "SELECT SETVAL('Books_Book_ID_Seq', MAX(Book_ID)) FROM Books;";
+  pool.query(sql_sequence, [], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Successful creation of 3 books");
+  });
+});
+
+// Creating the Express server
+const app = express();
+
+// Server configuration
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Starting the server
+app.listen(3000, () => {
+  console.log("Server started (http://localhost:3000/) !");
+});
+
+// GET /
+app.get("/", (req, res) => {
+  // res.send("Hello world...");
+  res.render("index");
+});
+
+// GET /about
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// GET /data
+app.get("/data", (req, res) => {
+  const test = {
+    titre: "Test",
+    items: ["one", "two", "three"],
+  };
+  res.render("data", { model: test });
+});
+
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM Books ORDER BY Title";
+  pool.query(sql, [], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("books", { model: result.rows });
+  });
+});
